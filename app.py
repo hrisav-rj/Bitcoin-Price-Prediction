982c9dbb11cf17b7a90b6c38644346bf0180f536
Hrisav <103006770+hrisav-rj@users.noreply.github.com>
2024-05-02 00:07:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model("C:\\Users\\hp\Downloads\\finalProject\\Bitcoin Prediction Model.keras")
+model = load_model("Bitcoin Prediction Model.keras")
 
 st.header('Bitcoin Price Predictor')
 
